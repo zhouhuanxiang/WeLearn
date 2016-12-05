7d044b86942a62c81269616a1c5d82a230517474
Yjing <18569095589@163.com>
2016-12-05 16:38:04 +0800

    课程信息 课程表 文图座位

---
+++ routes/lesson/notices.js
@@ -0,0 +1,51 @@
+var wrapper = require('../../wrapper');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var utf8 = require('utf8');
+var router = express.Router();
+var Student = require('../../Models/Student');
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+router.get('/', function (req, res, next) {
+    //console.log(req.baseUrl);
+    var lesson_id = req.baseUrl.split("/");
+    lesson_id = lesson_id[3];
+    var requestData = {
+        apikey: "",
+        apisecret: ""
+    };
+
+    Student.find({}, function(err,docs){
+        if(err) next(err);
+        //console.log(docs);
+
+        var username = docs[0].studentnumber;
+        request({
+            method: 'POST',
+            url: 'http://se.zhuangty.com:8000/learnhelper/' + username + '/courses/' + lesson_id + '/notices',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        }, function (error, response, body) {
+            if(response.statusCode === 200) {
+                var notices = JSON.parse(body);
+                //console.log(notices);
+
+                res.render('lesson/notices', {
+                    title: '课程公告',
+                    notices: notices
+                });
+            }
+            else {
+                console.log('error response.');
+            }
+
+        });
+    });
+});
+
+module.exports = router;
---
+++ routes/library/seat.js
@@ -0,0 +1,39 @@
+var wrapper = require('../../wrapper');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var utf8 = require('utf8');
+var router = express.Router();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+router.get('/', function (req, res, next) {
+    var requestData = {
+        apikey: "",
+        apisecret: ""
+    };
+    request({
+        method: 'POST',
+        url: 'http://se.zhuangty.com:8000/library/hs',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    }, function (error, response, body) {
+        if(response.statusCode === 200) {
+            var seats = JSON.parse(body);
+            console.log(seats);
+
+            res.render('library/seat', {
+                title: '文图座位信息一览表'
+            });
+        }
+        else {
+            console.log('error response.');
+        }
+
+    });
+});
+
+module.exports = router;
